78dad37e3921917697d31f179aefc0b046747037
trisselino <tristan.sopauschke@gmail.com>
2024-12-04 01:18:39 +0100

    day 3 second star

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -1,23 +1,38 @@
 import scala.io.Source
-import scala.util.matching.Regex
 
 @main
 def day3() : Unit =
+    // part one
     val input = Source.fromResource("input3.txt").getLines().toArray
 
     val inputRegex = """mul\((\d{1,3}),(\d{1,3})\)""".r
-    val mulMatches = input.flatMap (line => inputRegex.findAllMatchIn(line).map(x => x.matched) // (_.matched)
-    )
+    val mulMatches = input.flatMap(line => inputRegex.findAllMatchIn(line).map(x => x.matched)) // (_.matched)
 
     val valueRegex = """(\d{1,3})""".r
-    val numbers: Array[Int] = mulMatches.flatMap(str => valueRegex.findAllMatchIn(str).map(_.matched.toInt))
+    val values: Array[Int] = mulMatches.flatMap(str => valueRegex.findAllMatchIn(str).map(_.matched.toInt))
 
-    val sum = numbers.grouped(2).collect{
+    val sum = values.grouped(2).collect{
         case Array(a,b) => a * b
     }.sum
     printf("Sum: %d\n", sum)
 
+    //part two
+    val regex2 = """don't\(\).*?do\(\)""".r
+    val firstfilteredInput = regex2.replaceAllIn(input.mkString, "")
+    val dontEOF = """don't\(\).*""".r
+    val filteredInput = {
+        // necessary because don't() appears two times before EOF in input3 -> not a universal solution!
+        val index = firstfilteredInput.lastIndexOf("don't()")
+        val upToLastOcc = firstfilteredInput.substring(0, index)
+        val secondToLastIndex = upToLastOcc.lastIndexOf("don't()")
+        val upToSecondLastOcc = upToLastOcc.substring(0, secondToLastIndex)
+        upToSecondLastOcc
+    }
 
-//  val matches = input.flatMap { line =>
-//    regex.findAllMatchIn(line).map(_.matched)
-//  }
+    // the rest is practically the same as in part one
+    val filteredMatches = inputRegex.findAllMatchIn(filteredInput).map(_.matched).toArray
+    val filteredValues =  filteredMatches.flatMap(str => valueRegex.findAllMatchIn(str).map(_.matched.toInt))
+    val filteredSum = filteredValues.grouped(2).collect{
+        case Array(x,y) => x * y
+    }.sum
+    printf("Filter Sum: %d\n", filteredSum)
